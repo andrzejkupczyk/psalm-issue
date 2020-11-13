92a4a8d217e7cb74efd359007f8d43e2a50fa640
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2020-11-13 11:15:08 +0100

    result 2

---
+++ src/Laravel/ServiceProvider.php
@@ -0,0 +1,13 @@
+ServiceProvider?php
+
+namespace Ns1\Ns2\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+}
+void
